55c9cfa06ce08082a83edec4c280bcab1957d4ea
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-11-25 13:29:58 +0200

    1. Add Sliding (Text) Animation to Splash View

--- lib/features/splash/presentation/views/splash_view.dart
+++ lib/features/splash/presentation/views/splash_view.dart
@@ -13,15 +13,55 @@ class SplashView extends StatelessWidget {
   }
 }
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
 
   @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController animationController;
+  late final Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 10), end: const Offset(0, 0))
+            .animate(animationController);
+
+    animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Image(
-        image: AssetImage(kLogoImagePath),
-      ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        const Image(image: AssetImage(kLogoImagePath)),
+        AnimatedBuilder(
+          animation: slidingAnimation,
+          builder: (context, _) => SlideTransition(
+            position: slidingAnimation,
+            child: const Text('Read Free Books', textAlign: TextAlign.center),
+          ),
+        ),
+      ],
     );
   }
 }
