8427b6394c735443d139c76d3326fb382497b0e4
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-11-25 18:29:23 +0200

    9. Split App Themes into Separated themes.dart File

---
+++ lib/core/constants/themes.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import 'decorations.dart';
+
+const kAppBarTheme = AppBarTheme(
+  elevation: 0,
+  backgroundColor: Colors.transparent,
+);
+
+const kCardTheme = CardTheme(
+  margin: EdgeInsets.zero,
+  clipBehavior: Clip.hardEdge,
+  shape: kRoundedRectangleBorder12,
+);
--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 import 'core/constants/colors.dart';
-import 'core/constants/decorations.dart';
+import 'core/constants/themes.dart';
 import 'features/splash/presentation/views/splash_view.dart';
 
 void main() => runApp(const Readily());
@@ -17,15 +17,8 @@ class Readily extends StatelessWidget {
       theme: ThemeData(
         brightness: Brightness.dark,
         scaffoldBackgroundColor: kScaffoldBackgroundColor,
-        appBarTheme: const AppBarTheme(
-          elevation: 0,
-          backgroundColor: Colors.transparent,
-        ),
-        cardTheme: const CardTheme(
-          margin: EdgeInsets.zero,
-          clipBehavior: Clip.hardEdge,
-          shape: kRoundedRectangleBorder12,
-        ),
+        appBarTheme: kAppBarTheme,
+        cardTheme: kCardTheme,
       ),
       home: const SplashView(),
     );
