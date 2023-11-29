5f1a291145458c693731b34bc9afdcc7e9313482
Fady Fawzy <FadyFawzy.Official@gmail.com>
2023-11-29 16:49:26 +0200

    1. Add SearchView to AppRouter & Navigate to it from Home

---
+++ lib/features/search/presentation/views/search_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SearchView extends StatelessWidget {
+  static const name = '/search';
+
+  const SearchView({super.key});
+
+  @override
+  Widget build(context) {
+    return const Scaffold();
+  }
+}
