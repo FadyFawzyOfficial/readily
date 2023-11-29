30a8cb5fdc034a667463a58d109b234fca52a530
Fady Fawzy <FadyFawzy.Official@gmail.com>
2023-11-29 12:23:33 +0200

    2. Customize BookDetailsView AppBar Design

--- lib/features/home/presentation/views/book_details_view.dart
+++ lib/features/home/presentation/views/book_details_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class BookDetailsView extends StatelessWidget {
   static const name = '/bookDetails';
@@ -7,7 +8,18 @@ class BookDetailsView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: context.pop,
+          icon: const Icon(Icons.close_rounded),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.shopping_cart_rounded),
+          ),
+        ],
+      ),
     );
   }
 }
