7a3a31fab197ae7de9d419569c85e1c80a5fb38d
Fady Fawzy <FadyFawzy.Official@gmail.com>
2023-12-05 14:18:55 +0200

    5. Create fetchNewestBooks() Method to get books & emit different states

--- lib/features/home/presentation/cubits/newest_books/newest_books_cubit.dart
+++ lib/features/home/presentation/cubits/newest_books/newest_books_cubit.dart
@@ -2,9 +2,23 @@ import 'package:equatable/equatable.dart' show Equatable;
 import 'package:flutter_bloc/flutter_bloc.dart' show Cubit;
 
 import '../../../data/models/book/book.dart';
+import '../../../data/repos/home_repo.dart';
 
 part 'newest_books_state.dart';
 
 class NewestBooksCubit extends Cubit<NewestBooksState> {
-  NewestBooksCubit() : super(NewestBooksInitial());
+  final HomeRepo homeRepo;
+
+  NewestBooksCubit(this.homeRepo) : super(NewestBooksInitial());
+
+  Future<void> fetchNewestBooks() async {
+    emit(NewestBooksLoading());
+
+    final result = await homeRepo.fetchNewestBooks();
+
+    result.fold(
+      (failure) => emit(NewestBooksFailure(message: failure.message)),
+      (books) => NewestBooksSuccess(books: books),
+    );
+  }
 }
