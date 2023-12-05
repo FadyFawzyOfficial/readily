eaeff85fb18952af300ebba79543a8cfd43a2976
Fady Fawzy <FadyFawzy.Official@gmail.com>
2023-12-05 14:10:42 +0200

    3. Create fetchFeaturedBooks() Method to get books & emit different states

--- lib/features/home/presentation/cubits/featured_books/featured_books_cubit.dart
+++ lib/features/home/presentation/cubits/featured_books/featured_books_cubit.dart
@@ -1,9 +1,24 @@
-import 'package:bloc/bloc.dart';
-import 'package:equatable/equatable.dart';
-import 'package:readily/features/home/data/models/book/book.dart';
+import 'package:equatable/equatable.dart' show Equatable;
+import 'package:flutter_bloc/flutter_bloc.dart' show Cubit;
+
+import '../../../data/models/book/book.dart';
+import '../../../data/repos/home_repo.dart';
 
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  final HomeRepo homeRepo;
+
+  FeaturedBooksCubit(this.homeRepo) : super(FeaturedBooksInitial());
+
+  Future<void> fetchFeaturedBooks() async {
+    emit(FeaturedBooksLoading());
+
+    final result = await homeRepo.fetchFeaturedBooks();
+
+    result.fold(
+      (failure) => emit(FeaturedBooksFailure(message: failure.message)),
+      (books) => FeaturedBooksSuccess(books: books),
+    );
+  }
 }
