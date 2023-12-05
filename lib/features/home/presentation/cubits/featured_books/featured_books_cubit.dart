e5ca618a0267bfe9d894029538ccc9a0ad053433
Fady Fawzy <FadyFawzy.Official@gmail.com>
2023-12-05 14:04:11 +0200

    2. Create FeaturedBooks Cubit with its States

---
+++ lib/features/home/presentation/cubits/featured_books/featured_books_cubit.dart
@@ -0,0 +1,9 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:readily/features/home/data/models/book/book.dart';
+
+part 'featured_books_state.dart';
+
+class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
+  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+}
