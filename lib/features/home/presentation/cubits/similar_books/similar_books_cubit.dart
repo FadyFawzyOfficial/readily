dfb4e425907aa55231380ace993eec1f784a33df
Fady Fawzy <FadyFawzy.Official@gmail.com>
2023-12-06 18:15:30 +0200

    3. Create fetchSimilarBooks() Method to get books & emit different states

--- lib/features/home/presentation/cubits/similar_books/similar_books_cubit.dart
+++ lib/features/home/presentation/cubits/similar_books/similar_books_cubit.dart
@@ -2,9 +2,23 @@ import 'package:equatable/equatable.dart' show Equatable;
 import 'package:flutter_bloc/flutter_bloc.dart' show Cubit;
 
 import '../../../data/models/book/book.dart';
+import '../../../data/repos/home_repo.dart';
 
 part 'similar_books_state.dart';
 
 class SimilarBooksCubit extends Cubit<SimilarBooksState> {
-  SimilarBooksCubit() : super(SimilarBooksInitial());
+  final HomeRepo homeRepo;
+
+  SimilarBooksCubit(this.homeRepo) : super(SimilarBooksInitial());
+
+  Future<void> fetchSimilarBooks({required String category}) async {
+    emit(SimilarBooksLoading());
+
+    final result = await homeRepo.fetchSimilarBooks(category: category);
+
+    result.fold(
+      (failure) => emit(SimilarBooksFailure(message: failure.message)),
+      (books) => SimilarBooksSuccess(books: books),
+    );
+  }
 }
