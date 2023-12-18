6933e6f13bff81a9312935fdcc9ecb57644351a2
Fady Fawzy <FadyFawzy.Official@gmail.com>
2023-12-18 10:16:25 +0200

    2. Implement fetchFeaturedBooks() in HomeRepoImpl class

--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -3,12 +3,29 @@ import 'package:dartz/dartz.dart';
 import '../../../../core/exceptions/app_exception.dart';
 import '../../domain/entities/book_entity.dart';
 import '../../domain/repos/home_repo.dart';
+import '../data_sources/home_local_data_source.dart';
+import '../data_sources/home_remote_data_source.dart';
 
 class HomeRepoImpl extends HomeRepo {
+  final HomeRemoteDataSource homeRemoteDataSource;
+  final HomeLocalDataSource homeLocalDataSource;
+
+  HomeRepoImpl({
+    required this.homeRemoteDataSource,
+    required this.homeLocalDataSource,
+  });
+
   @override
-  Future<Either<AppException, List<BookEntity>>> fetchFeaturedBooks() {
-    // TODO: implement fetchFeaturedBooks
-    throw UnimplementedError();
+  Future<Either<AppException, List<BookEntity>>> fetchFeaturedBooks() async {
+    try {
+      var books = homeLocalDataSource.fetchFeaturedBooks();
+      if (books.isNotEmpty) return right(books);
+
+      books = await homeRemoteDataSource.fetchFeaturedBooks();
+      return right(books);
+    } catch (e) {
+      return left(AppException());
+    }
   }
 
   @override
