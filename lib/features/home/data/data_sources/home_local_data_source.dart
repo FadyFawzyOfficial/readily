565b74421f543ae03e540bfa2a5a3f6728963703
Fady Fawzy <FadyFawzy.Official@gmail.com>
2023-12-18 09:34:33 +0200

    3. Build Home Local Data Source

---
+++ lib/features/home/data/data_sources/home_local_data_source.dart
@@ -0,0 +1,20 @@
+import '../../domain/entities/book_entity.dart';
+
+abstract class HomeLocalDataSource {
+  List<BookEntity> fetchFeaturedBooks();
+  List<BookEntity> fetchNewestBooks();
+}
+
+class HomeLocalDataSourceImpl extends HomeLocalDataSource {
+  @override
+  List<BookEntity> fetchFeaturedBooks() {
+    // TODO: implement fetchFeaturedBooks
+    throw UnimplementedError();
+  }
+
+  @override
+  List<BookEntity> fetchNewestBooks() {
+    // TODO: implement fetchNewestBooks
+    throw UnimplementedError();
+  }
+}
