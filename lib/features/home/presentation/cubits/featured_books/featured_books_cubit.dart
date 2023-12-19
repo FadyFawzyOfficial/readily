09cc08001a96b9c264b486300cb9fe3b21af2291
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-12-19 09:17:52 +0200

    3. Add Method to get or fetch featuredBooks() to Cubit

--- lib/features/home/presentation/cubits/featured_books/featured_books_cubit.dart
+++ lib/features/home/presentation/cubits/featured_books/featured_books_cubit.dart
@@ -1,9 +1,25 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../domain/entities/book_entity.dart';
+import '../../../domain/use_cases/fetch_featured_books_use_case.dart';
 
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  final FetchFeaturedBooksUseCase _featuredBooksUseCase;
+
+  FeaturedBooksCubit({required FetchFeaturedBooksUseCase featuredBooksUseCase})
+      : _featuredBooksUseCase = featuredBooksUseCase,
+        super(FeaturedBooksInitial());
+
+  Future<void> get featuredBooks async {
+    emit(FeaturedBooksLoading());
+
+    final result = await _featuredBooksUseCase.call();
+
+    result.fold(
+      (error) => emit(FeaturedBooksFailure(message: error.message)),
+      (books) => emit(FeaturedBooksSuccess(books: books)),
+    );
+  }
 }
